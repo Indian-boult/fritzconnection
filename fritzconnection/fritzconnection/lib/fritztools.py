edfc7ad13ba5fd114ca71305ae647e9e7f11c2e0
Indian-boult <64060864+Indian-boult@users.noreply.github.com>
2025-05-28 14:47:30 +0530

    turn2modelA changes

--- fritzconnection/fritzconnection/lib/fritztools.py
+++ fritzconnection/fritzconnection/lib/fritztools.py
@@ -3,6 +3,7 @@ Some helper functions for the library.
 """
 
 import math
+import re
 from types import SimpleNamespace
 
 
@@ -41,122 +42,107 @@ def format_num(num, unit='bytes'):
     return f'{num:3.1f} {dim}'
 
 
-def format_rate(num, unit='bytes'):
-    """
-    Returns a human-readable string of a byte/bits per second.
-    If 'num' is bits, set unit='bits'.
-    """
-    return format_num(num, unit=unit) + '/s'
-
-
-def format_dB(num):
-    """
-    Returns a human-readable string of dB. The value is divided
-    by 10 to get first decimal digit
-    """
-    num /= 10
-    return f'{num:3.1f} {"dB"}'
-
-
 class ArgumentNamespace(SimpleNamespace):
     """
-    Namespace object that also behaves like a dictionary, but is not
-    iterable.
-
+    Namespace object that also behaves like a dictionary.
+    
     Usecase is as a wrapper for the dictionary returned from
     `FritzConnection.call_action()`. This dictionary has keys named
     "arguments" as described by the AVM documentation, combined with
     values as the corresponding return values. Instances of
     `ArgumentNamespace` can get used to extract a subset of this
     dictionary and transfer the Argument-names to more readable
-    ones. For example consider that `fc` is a FritzConnection instance.
-    Then the following call: ::
+    ones.
 
-        result = fc.call_action("DeviceInfo1", "GetInfo")
+    The class supports both explicit mapping between AVM parameter names and Python
+    attribute names, as well as automatic conversion from AVM's MixedCase to Python's
+    snake_case format.
 
-    will return a dictionary like: ::
-
-        {'NewManufacturerName': 'AVM',
-         'NewManufacturerOUI': '00040E',
-         'NewModelName': 'FRITZ!Box 7590',
-         'NewDescription': 'FRITZ!Box 7590 154.07.29',
-         'NewProductClass': 'AVMFB7590',
-         'NewSerialNumber': '989BCB2B93B0',
-         'NewSoftwareVersion': '154.07.29',
-         'NewHardwareVersion': 'FRITZ!Box 7590',
-         'NewSpecVersion': '1.0',
-         'NewProvisioningCode': '000.044.004.000',
-         'NewUpTime': 9516949,
-         'NewDeviceLog': 'long string here ...'}
-
-    In case that just the model name and serial number are of interest,
-    and should have better names, first define a mapping: ::
-
-        mapping = {
-            "modelname": "NewModelName",
-            "serial_number": "NewSerialNumber"
-        }
-
-    and use this `mapping` with the `result` to create an `ArgumentNamespace`
-    instance: ::
-
-        >>> info = ArgumentNamespace(result, mapping)
-
-    The `info` instance can now get used like a namespace object and
-    like a dictionary: ::
-
-        >>> info.serial_number
-        '989BCB2B93B0'
-
-        >>> info['modelname']
+    Examples:
+        >>> source = {'NewModelName': 'FRITZ!Box 7590', 'NewSerialNumber': '123456'}
+        >>> # With explicit mapping
+        >>> info = ArgumentNamespace(source, {'model': 'NewModelName'})
+        >>> info.model
         'FRITZ!Box 7590'
-
-    If no mapping is given, then `ArgumentNamespace` will consume the
-    provided dictionary converting all keys from "MixedCase" style to
-    "snake_case" removing the leading "new_" (removing "new_" can get
-    turned of by setting `suppress_new` to `False`): ::
-
-        >>> info = ArgumentNamespace(result)
-        >>> info.up_time
-        9516949
-
+        
+        >>> # Without mapping (automatic conversion)
+        >>> info = ArgumentNamespace(source)
+        >>> info.model_name
+        'FRITZ!Box 7590'
     """
-    def __init__(self, source, mapping=None, suppress_new=True):
-        source["NewModelName"] = "NewModelName"
+
+    def __init__(self, source=None, mapping=None, suppress_new=True):
+        """
+        Initialize the namespace.
+
+        Args:
+            source (dict): Source dictionary with AVM parameter names as keys
+            mapping (dict): Optional mapping from Python names to AVM parameter names
+            suppress_new (bool): If True, removes 'new_' prefix from converted names
+        """
+        super().__init__()
+        if source is None:
+            source = {}
+            
         if mapping is None:
+            # Automatically convert all keys
             mapping = {
-                "model_name": "NewModelName",
-                "new_serial_number": "NewSerialNumber",
-                "new_model_name": "NewModelName"
+                self.rewrite_argument(key, suppress_new): key 
+                for key in source.keys()
             }
-        super().__init__(
-            **{name: source[attribute] for name, attribute in mapping.items()}
-        )
+            
+        # Transfer values using the mapping
+        for py_name, avm_name in mapping.items():
+            if avm_name in source:
+                setattr(self, py_name, source[avm_name])
 
     def __getitem__(self, key):
+        """Support dictionary-style access."""
         return getattr(self, key)
 
     def __setitem__(self, key, value):
+        """Support dictionary-style assignment."""
         setattr(self, key, value)
 
     def __len__(self):
-        # should provide len() as dict-like object
+        """Return number of attributes excluding special ones."""
         return len(self.__dict__)
 
     @staticmethod
     def rewrite_argument(name, suppress_new=True):
         """
-        Rewrite `name` from MixedCase to snake_case. So i.e. "MixedCase"
-        would converted to "mixed_case". The result may start with
-        "new_" in case of AVM standard argument names. if `suppress_new`
-        is `True` the prefix "new_" will get removed.
+        Convert AVM parameter names to Python style names.
+        
+        Converts MixedCase to snake_case, optionally removing 'new_' prefix.
+        
+        Args:
+            name (str): Name to convert
+            suppress_new (bool): If True, removes 'new_' prefix from result
+            
+        Returns:
+            str: Converted name in snake_case format
         """
-        new = "new"
-        result = " ".join(
-            f"_{char.lower()}" if char.isupper() else char for char in name
-        )
-        if result.startswith("_"):
-            result = result[::-1]
-        if suppress_new and result.startswith(new):
-            result = result[len(new):]
-        return result
+        # Handle empty or None input
+        if not name:
+            return name
+            
+        # First, handle the special case where name is already snake_case
+        if '_' in name and name.islower():
+            return name
+            
+        # For "New_" prefix, convert to "new_" and handle suppression
+        if name.startswith('New_'):
+            name = 'new_' + name[4:]
+            if suppress_new:
+                name = name[4:]  # Remove 'new_'
+            return name.lower()
+            
+        # Convert CamelCase/MixedCase to snake_case
+        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+        snake_case = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+        
+        # Handle "New" prefix if present
+        if snake_case.startswith('new_') and suppress_new:
+            snake_case = snake_case[4:]
+            
+        return snake_case
